ef83551101fdfb6d4ea3600fd4ccd85453027cb3
Isis Zapata <isin_zapataf@unicah.edu>
2021-11-22 21:49:52 -0600

    a
    a

--- ProyectoGrupo6/app/src/main/java/com/example/proyectogrupo6/MainX0.java
+++ ProyectoGrupo6/app/src/main/java/com/example/proyectogrupo6/MainX0.java
@@ -2,7 +2,10 @@ package com.example.proyectogrupo6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainX0 extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainX0 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_x0);
+
     }
+    //Metodo para iniciar el juego x0
+    public void iniciarjuego(View view)
+    {
+        Intent iniciarjuego = new Intent(this,JuegoX0.class);
+        startActivity(iniciarjuego);
+    }
+
+    //Metodo para salir
+    public void regresar(View view)
+    {
+        Intent regresar = new Intent(this,MainActivity.class);
+        startActivity(regresar);
+    }
+
 }
